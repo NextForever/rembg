390115643c2e8b4dbaa74ed6420d3ca38dec640b
GezMin <s.shvetsov.v@gmail.com>
2022-10-21 19:05:33 +0600

    finished_application

--- Home.py
+++ Home.py
@@ -3,15 +3,15 @@ from rembg import remove
 from PIL import Image
 import numpy as np
 
-st.title("rembg Demo")
+st.title("Убираем фон с картинок")
 col1, col2 = st.columns(2)
-images = st.sidebar.file_uploader("Load Image", accept_multiple_files =True)
+images = st.sidebar.file_uploader("Загрузить фото", accept_multiple_files =True)
 if images:
     for image in images:
         with Image.open(image) as img:
-            col1.header("Original")
+            col1.header("Исходник")
             col1.image(img)
 
             output = remove(img)
-            col2.header("Extracted")
+            col2.header("Итог")
             col2.image(output)
